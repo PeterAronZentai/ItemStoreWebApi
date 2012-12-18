f53bdb335935b240b3d9cf612f065a6db36f8c82
Peter Aron Zentai <peter.zentai@jaystack.com>
2012-12-18 14:57:54 +0100

    fc

---
+++ WebApiJayData/Controllers/TodosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApiJayData.Models;
+
+namespace WebApiJayData.Controllers
+{
+    public class TodosController : ApiController
+    {
+        private TodoModelContainer db = new TodoModelContainer();
+
+        // GET api/Todos
+        public IQueryable<Todo> GetTodoes()
+        {
+            return db.Todoes;
+        }
+
+        // GET api/Todos/5
+        public Todo GetTodo(int id)
+        {
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return todo;
+        }
+
+        // PUT api/Todos/5
+        public HttpResponseMessage PutTodo(int id, Todo todo)
+        {
+            if (ModelState.IsValid && id == todo.Id)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Todos
+        public HttpResponseMessage PostTodo(Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Todoes.Add(todo);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, todo);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = todo.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Todos/5
+        public HttpResponseMessage DeleteTodo(int id)
+        {
+            Todo todo = db.Todoes.Find(id);
+            if (todo == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Todoes.Remove(todo);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, todo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
